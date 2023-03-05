921e7e53b7e10cbad48b020fa5808ce8934d2505
incode-mike-bytenko <michael.butenko@incode-it.com>
2023-03-05 16:49:11 +0200

    feat: Added new architecture mode, updated rn to latest -v

--- App.tsx
+++ App.tsx
@@ -10,24 +10,33 @@
 
 import React from 'react';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
-
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import {QueryClient, QueryClientProvider} from 'react-query';
+// import {Provider} from 'react-redux';
+// import {setupListeners} from '@reduxjs/toolkit/query';
 
 import Apollo from './src/containers/Core/Apollo/Apollo';
+// import store from './src/containers/Core/redux/store/store';
 
 import Navigation from './src/containers/Core/navigation/Navigation';
 
 const App = () => {
   const queryClient = new QueryClient();
 
+  // setupListeners(store.dispatch);
+
   return (
-    <SafeAreaProvider>
-      <QueryClientProvider client={queryClient}>
-        <Apollo>
-          <Navigation />
-        </Apollo>
-      </QueryClientProvider>
-    </SafeAreaProvider>
+    <GestureHandlerRootView style={{flex: 1}}>
+      <SafeAreaProvider>
+        {/* <Provider store={store}> */}
+        <QueryClientProvider client={queryClient}>
+          <Apollo>
+            <Navigation />
+          </Apollo>
+        </QueryClientProvider>
+        {/* </Provider> */}
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
   );
 };
 
